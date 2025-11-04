d00ecdc7d9c303c9d8aaa809a2b7358f92750d86
YYInGitHub <cheng_yew_yih@myrp.edu.sg>
2025-11-04 11:57:35 +0800

    Update db.js (5th)

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL database:', err);
         console.log('Make sure your.env file is correct and try again.');
-        console.log('Nice DB Connection ...');
+        console.log('Database Connection ...');
         return;
     }
     console.log('Connected to MySQL database');
